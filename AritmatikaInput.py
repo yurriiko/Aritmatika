b92011a80f886081911a5b6cac7f64b7019974e7
yurriiko <77604678+yurriiko@users.noreply.github.com>
2021-01-18 15:16:19 +0700

    Add files via upload

---
+++ AritmatikaInput.py
@@ -0,0 +1,26 @@
+A = 8
+B = 3
+C = 2
+print ("==== Program Aritmatika ====")
+print (" Nilai A = 8")
+print (" Nilai B = 3")
+print (" Nilai C = 2")
+print ("==== Hasil Perhitungan ====")
+hasil = A + B
+print ("A + B = ",hasil)
+hasil = A - B
+print ("A - B = ",hasil)
+hasil = A * B
+print ("A * B = ",hasil)
+hasil = A / B
+print ("A / B = ",hasil)
+hasil = A % B 
+print ("A % B = ",hasil)
+hasil = A // B
+print ("A // B = ",hasil)
+hasil = A ** B
+print ("A ** B = ",hasil)
+hasil = (A-B)/C
+print ("(A - B) / C = ",hasil)
+hasil = A-B/C
+print ("A - B / C = ",hasil)
